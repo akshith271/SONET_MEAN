7374de261cb35f94b713f9c81af4070ffb1925e6
Akshith Bharadwaj <akshithbharadwaj@gmail.com>
2021-06-29 00:27:49 +0530

    Assigment solution

--- todo/app.js
+++ todo/app.js
@@ -1,94 +1,85 @@
 window.onload = () => {
-    //on load
-    const form1 = document.querySelector("#addForm");
+    const form1 = document.querySelector('#addForm');
 
-    let items = document.getElementById("items");
-    let submit = document.getElementById("submit");
+    let items = document.getElementById('items');
+    let submit = document.getElementById('submit');
 
-    let editItem = null;
+    form1.addEventListener('submit', addItem);
 
-    //listeners to be added here
+    function addItem(e) {
+        e.preventDefault();
+        if (submit.value === 'EDIT') {
+            const liId = submit.dataset.currentItemId;
+            const li = document.getElementById(liId);
+            const inputEle = document.getElementById('item');
+
+            li.dataset.content = inputEle.value;
+            li.childNodes[0].data = inputEle.value;
+
+            submit.value = 'Submit';
+            inputEle.value = '';
+
+            const success = document.getElementById('lblsuccess');
+            success.innerHTML = 'Text edited successfully';
+
+            success.style.display = 'block';
+
+            setTimeout(function () {
+                success.style.display = 'none';
+            }, 3000);
+
+            return;
+        }
+
+        let newItem = document.getElementById('item').value;
+        if (!newItem.trim()) return;
+
+        document.getElementById('item').value = '';
+
+        const index = items.children.length;
+        items.innerHTML += `
+        <li class="list-group-item" id="${index}" data-content="${newItem}">
+                ${newItem}
+            <button class="btn-danger btn btn-sm float-right delete" onclick="removeItem(${index})">
+                Delete
+            </button>
+            <button class="btn-success btn btn-sm float-right edit" onclick="editItem(${index})">
+                Edit
+            </button>
+        </li>
+        `;
+    }
 };
 
-function addItemm(e) {
-    e.preventDefault();
+function toggleButton(inputEle, buttonId) {
+    const submitButton = document.getElementById(buttonId);
+    const { value } = inputEle;
+    submitButton.disabled = !value.trim();
+}
 
-    if (submit.value !== "Submit") {
-        console.log("Hello");
+function removeItem(id) {
+    const li = document.getElementById(id);
+    const items = document.getElementById('items');
+    if (confirm('Are you Sure?')) {
+        items.removeChild(li);
+        const success = document.getElementById('lblsuccess');
 
-        editItem.target.parentNode.childNodes[0].data
-            = document.getElementById("item").value;
+        success.innerHTML = 'Text deleted successfully';
 
-        submit.value = "Submit";
-        document.getElementById("item").value = "";
-
-        document.getElementById("lblsuccess").innerHTML
-            = "Text edited successfully";
-
-        document.getElementById("lblsuccess")
-            .style.display = "block";
-
-        setTimeout(function() {
-            document.getElementById("lblsuccess")
-                .style.display = "non";
+        success.style.display = 'block';
+        setTimeout(function () {
+            success.style.display = 'none';
         }, 3000);
-
-        return false;
-    }
-
-    let newItem = document.getElementById("item").value;
-    if (newItem.trim() == "" || newItem.trim() == null)
-        return false;
-    else
-        document.getElementById("item").value = "";
-
-    let li = document.createElement("li");
-    li.className = "list-group-item";
-
-    let deleteButton = document.createElement("button");
-
-    deleteButton.className =
-        "btn-danger btn btn-sm float-right delete";
-
-    deleteButton.appendChild(document.createTextNode("Delete"));
-
-    let editButton = document.createElement("button");
-
-    editButton.className =
-        "btn-success btn btn-sm float-right edit";
-
-    editButton.appendChild(document.createTextNode("Edit"));
-
-    li.appendChild(document.createTextNode(newItem));
-    li.appendChild(deleteButton);
-    li.appendChild(editButton);
-
-    items.appendChild(li);
-}
-
-function removeItem(e) {
-    e.preventDefault();
-    if (e.target.classList.contains("delete")) {
-        if (confirm("Are you Sure?")) {
-            let li = e.target.parentNode;
-            items.removeChild(li);
-            document.getElementById("success").innerHTML
-                = "Text deleted successfully";
-
-            document.getElementById("lblsuccess")
-                .style.display = "block";
-            setTimeout(function() {
-                document.getElementById("lblsuccess")
-                    .style.display = "none";
-            }, 3000);
-        }
-    }
-    if (e.target.classList.contains("edit")) {
-        document.getElementById("item").value =
-            e.target.parentNode.childNodes[0].data;
-        submit.value = "EDIT";
-        editItem = e;
     }
 }
 
-//enable submit button here
+function editItem(id) {
+    const li = document.getElementById(id);
+    const inputEle = document.getElementById('item');
+    inputEle.value = li.dataset.content;
+
+    const submitButton = document.getElementById('submit');
+    submitButton.value = 'EDIT';
+    submitButton.dataset.currentItemId = id;
+}
+
